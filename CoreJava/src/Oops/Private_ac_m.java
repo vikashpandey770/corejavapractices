26b52c21d3b19dc51dc3eff992f1eb0f4951e038
vikashpandey770 <vikashshrinivaspandey@gmail.com>
2026-07-29 23:27:28 +0530

    addhtmlfiles

---
+++ CoreJava/src/Oops/Private_ac_m.java
@@ -0,0 +1,32 @@
+package Oops;
+
+class Pri{
+  private void ages(){
+        System.out.println("my age is 25 year old");
+        System.out.println("this is a private access modifires");
+    }
+
+public void sh(){
+      ages();
+}
+}
+
+class Nex{
+    void addd(){
+        System.out.println("this is a not call from private property from another class");
+
+    }
+
+}
+
+public class Private_ac_m {
+    public static void main(String[] args) {
+        System.out.println("this is a private access modifires");
+
+        Nex n=new Nex();
+        n.addd();
+
+        Pri pr=new Pri();
+        pr.sh();
+    }
+}
